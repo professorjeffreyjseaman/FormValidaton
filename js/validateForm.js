faa4d0a7232daa65db2d1b02e12d3550ca880abb
Jeff Seaman <professorjeffreyjseaman@gmail.com>
2024-03-18 19:28:27 -0400

    adding 2nd Iteration to form validation by adding color red to textboxes and error message

--- js/validateForm.js
+++ js/validateForm.js
@@ -6,21 +6,28 @@ function validateForm() {
     document.getElementById('nameError').innerHTML = '';
     document.getElementById('emailError').innerHTML = '';
 
+    document.getElementById('name').classList.remove('error');
+    document.getElementById('email').classList.remove('error');
+
+
     var isValid = true;
 
     //Identify if name is empty
     if (name.trim() === ''){
         document.getElementById('nameError').innerHTML = 'Name is Required';
+        document.getElementById('name').classList.add('error');
         isValid = false;
     }
 
     if (email.trim() === ''){
         document.getElementById('emailError').innerHTML = 'Email is Required';
+        document.getElementById('email').classList.add('error');
         isValid = false;
     } else {
         var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailPattern.test(email)){
             document.getElementById('emailError').innerHTML = 'Invalid Email format';
+            document.getElementById('email').classList.add('error');
             isValid = false;
         }
     }
